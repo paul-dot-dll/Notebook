3b6acf61de49d177df489c83f7a6e552b5901c7a
paul-dot-dll <47700340+paul-dot-dll@users.noreply.github.com>
2021-04-11 18:37:10 +0100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notebook
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog SaveWindow = new SaveFileDialog();
+            SaveWindow.Filter = "Text Documents (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (SaveWindow.ShowDialog() == true)
+            {
+                File.WriteAllText(SaveWindow.FileName, TextB.Text);
+            }
+        }
+
+        private void LoadBTN_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog LoadWindow = new OpenFileDialog();
+            LoadWindow.Filter = "Text Documents (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (LoadWindow.ShowDialog() == true)
+            {
+                TextB.Text = File.ReadAllText(LoadWindow.FileName);
+            }
+        }
+
+        private void TextB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            return;
+        }
+    }
+}
